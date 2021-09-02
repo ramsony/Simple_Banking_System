f7563666d0c79c6c77d765fa3e11a143c2563975
Ramadhani Mdee <42729286+ramsony@users.noreply.github.com>
2021-09-02 18:39:46 -0400

    implementing Luhn algorithm.

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -1,61 +1,83 @@
 import random
 
-account_number = []
-pin_numbers = []
+
+def luhn_algo(numbers):
+    check_sum = 0
+    odd_numbers = numbers[-1::-2]
+    even_numbers = numbers[-2::-2]
+    check_sum += sum(odd_numbers)
+    for n in even_numbers:
+        temp = n * 2
+        if temp > 9:
+            check_sum += (temp - 9)
+        else:
+            check_sum += temp
+    return True if check_sum % 10 == 0 else False
 
 
-def card_number():
-    check_sum = '2'
-    iin = '400000'
+def generate_card_number():
+    bank_identification_number = [4, 0, 0, 0, 0, 0]
     ac = ''
-    customer_account_number = random.sample(range(9), 9)
-    for acc in customer_account_number:
-        ac += str(acc)
-    account_number.append(iin + ac + check_sum)
-    return iin + ac + check_sum
+    account_identifier = random.sample(range(0, 10), 10)
+    while True:
+        if not luhn_algo(bank_identification_number + account_identifier):
+            account_identifier = random.sample(range(0, 10), 10)
+        else:
+            card_number = bank_identification_number + account_identifier
+            break
+    for numbers in card_number:
+        ac += str(numbers)
+    return ac
 
 
-def pin_number():
-    pin = ''
+def generate_pin_number():
+    pin_ = ''
     pin_random = random.sample(range(9), 4)
     for pins in pin_random:
-        pin += str(pins)
-    pin_numbers.append(pin)
-    return pin
+        pin_ += str(pins)
+    return pin_
 
 
+class BankSystem:
+    def __init__(self):
+        self.credit_card = {}
+
+
+bs = BankSystem()
 while True:
     print('1. Create account')
     print('2. Log into account')
     print('0. Exit')
-    choice = input()
-    if choice == '1':
+    choice_menu_1 = input()
+    if choice_menu_1 == '1':
+        card_no = generate_card_number()
+        pin_no = generate_pin_number()
+        bs.credit_card[card_no] = pin_no
         print('Your card has been created')
         print('Your card number:')
-        print(card_number())
+        print(card_no)
         print('Your card PIN:')
-        print(pin_number())
-    if choice == '2':
-        card_no = input('Enter your card number:')
-        pin_no = input('Enter your PIN:')
-        if card_no not in account_number or pin_no not in pin_numbers:
-            print('Wrong card number or PIN!')
-        else:
+        print(pin_no)
+    if choice_menu_1 == '2':
+        card = input('Enter your card number:')
+        pin = input('Enter your PIN:')
+        if (card, pin) in bs.credit_card.items():
             print('You have successfully logged in!')
-            check2 = True
-            while check2:
+            while True:
                 print('1.Balance')
                 print('2.Log out')
                 print('0.Exit')
-                choice2 = input()
-                if choice2 == '1':
+                menu_choice_2 = input()
+                if menu_choice_2 == '1':
                     print('Balance: 0')
-                if choice2 == '2':
+                if menu_choice_2 == '2':
                     print('You have successfully logged out!')
-                    check2 = False
-                if choice2 == '0':
+                    break
+                if menu_choice_2 == '0':
                     print('Bye!')
                     exit()
-    if choice == '0':
+        else:
+            print('Wrong card number or PIN!')
+    if choice_menu_1 == '0':
         print('Bye!')
         break
